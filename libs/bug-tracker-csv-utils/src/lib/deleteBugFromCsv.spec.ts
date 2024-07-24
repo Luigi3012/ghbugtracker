b60be6ea2d792f621bfdf74a68ef2d51c1ae2988
Lukas Belaj <lukas.belaj@resco.net>
2024-07-24 17:08:01 +0200

    Fix csv utils test

--- libs/bug-tracker-csv-utils/src/lib/deleteBugFromCsv.spec.ts
+++ libs/bug-tracker-csv-utils/src/lib/deleteBugFromCsv.spec.ts
@@ -2,13 +2,15 @@ import * as fs from "fs";
 import { deleteBugFromCsv } from "./deleteBugFromCsv";
 
 describe("deleteBugFromCsv", () => {
-	jest
-		.spyOn(fs, "readFileSync")
-		.mockReturnValue(
-			"id,description,link,parentId,creationTimestamp,status\nBUG-001,Null pointer exception on login,http://example.com/bugs/BUG-001,,2024-07-20 10:15:00,Open\n"
-		);
+	beforeEach(() => {
+		jest
+			.spyOn(fs, "readFileSync")
+			.mockReturnValue(
+				"id,description,link,parentId,creationTimestamp,status\nBUG-001,Null pointer exception on login,http://example.com/bugs/BUG-001,,2024-07-20 10:15:00,Open\n"
+			);
 
-	jest.spyOn(fs, "writeFileSync").mockImplementation(() => true);
+		jest.spyOn(fs, "writeFileSync").mockImplementation(() => true);
+	});
 
 	it("should delete a existing bug from the CSV file", () => {
 		const wasDeleted = deleteBugFromCsv("BUG-001");
