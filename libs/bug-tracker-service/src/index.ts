446c2bf5497d7f43bfbe65a9b3fa2165fb158cb6
Lukas Belaj <lukas.belaj@resco.net>
2024-07-24 17:35:37 +0200

    Fix build

--- libs/bug-tracker-service/src/index.ts
+++ libs/bug-tracker-service/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./lib/deleteBugById";
-export * from "./lib/ghbugtracker-service";
 export * from "./lib/loadBugs";
 export * from "./lib/saveBug";
 export * from "./utils/createEmptyBug";
