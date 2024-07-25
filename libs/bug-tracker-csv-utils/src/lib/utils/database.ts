6a92baba0d6897c3de939961f26f508688bbd652
Lukas Belaj <lukas.belaj@resco.net>
2024-07-25 10:19:33 +0200

    Rework for docker support

--- libs/bug-tracker-csv-utils/src/lib/utils/database.ts
+++ libs/bug-tracker-csv-utils/src/lib/utils/database.ts
@@ -1,2 +1,4 @@
+import { config } from "dotenv";
+
 // In this exercise we won't use any database, just csv storage
-export const DB_FILE_PATH = "./db.csv";
+export const DB_FILE_PATH = config().parsed?.["DB_FILE_PATH"] || process.env["DB_FILE_PATH"] || "./db.csv";
